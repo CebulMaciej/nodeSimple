cc9e94143532757e6f2d47dc3cc7a9eaf71029f1
Maciej Cebula <maciejcebula96@gmail.com>
2021-10-15 12:45:13 +0200

    init

---
+++ server.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const bodyParser = require('body-parser')
+const expressJSDocSwagger = require('express-jsdoc-swagger');
+
+/**
+ * A client
+ * @typedef {object} LoginRequest
+ * @property {string} login.required -  login
+ * @property {string} password.required - password
+ */
+const options = {
+    info: {
+        version: '1.0.0',
+        title: 'OrientDB Connector',
+    },
+    baseDir: __dirname,
+    // Glob pattern to find your jsdoc files (multiple patterns can be added in an array)
+    filesPattern: './**/*.js',
+    // URL where SwaggerUI will be rendered
+    swaggerUIPath: '/api-docs',
+    // Expose OpenAPI UI
+    exposeSwaggerUI: true,
+    // Expose Open API JSON Docs documentation in `apiDocsPath` path.
+    exposeApiDocs: false,
+    // Open API JSON Docs endpoint.
+    apiDocsPath: '/v3/api-docs',
+    // Set non-required fields as nullable by default
+    notRequiredAsNullable: false,
+    // You can customize your UI options.
+    // you can extend swagger-ui-express config. You can checkout an example of this
+    // in the `example/configuration/swaggerOptions.js`
+    swaggerUiOptions: {},
+};
+
+const app = express();
+const PORT = 8091;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+
+function generateAccessToken(username) {
+    return jwt.sign({username: username}, "superExpressKey", { expiresIn: '1800s' });
+}
+/**
+ * GET /api/me
+ * @summary This is the summary of the endpoint
+ * @tags User
+ * @return {object} 200 - success response - application/json
+ * @return {object} 400 - Bad request response
+ */
+app.get('/api/me', async (req, res) => {
+
+    try
+    {
+        res.json({username: "admin"})
+    }
+    catch(ex){
+        res.statusCode = 500;
+        res.json(ex)
+    }
+});
+
+/**
+ * POST /api/login
+ * @summary This is the summary of the endpoint
+ * @tags User
+ * @param {LoginRequest} request.body - login request
+ * @return {object} 200 - success response - application/json
+ * @return {object} 400 - Bad request response
+ */
+app.post('/api/login', async (req, res) => {
+
+    try
+    {
+        let login = req.body.login;
+        let password = req.body.password;
+
+        if(login !== "admin" || password !== "admin"){
+            res.statusCode=400;
+            res.json(null);
+            return;
+        }
+
+        let result = generateAccessToken(login);
+
+        res.json(result);
+    }
+    catch(ex){
+        res.statusCode = 500;
+        res.json(ex)
+    }
+});
+expressJSDocSwagger(app)(options);
+
+app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`));
